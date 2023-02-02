50cb0601d99cdaf7d5b4219a782cb21f280d50a0
Riuseigi <troyliamjarata@gmail.com>
2023-02-02 10:08:36 +0800

    passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import './ExpenseItem.css'
-const ExpenseItem = () => {
-    const expenseDate = new Date(2023, 2, 2)
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.68
-    return (
-        <div className='expense-item '>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
-            </div>
-        </div>
+import React from "react";
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item ">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default ExpenseItem
+export default ExpenseItem;
