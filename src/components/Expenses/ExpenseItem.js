b9874bd8bd2ba74297358f4f71e03e9d99175e5d
Riuseigi <troyliamjarata@gmail.com>
2023-02-03 09:38:24 +0800

    add eventHandler and use useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Cards from "../UI/Cards";
 const ExpenseItem = (props) => {
+  //function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
   return (
     <Cards className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Cards>
   );
 };
